ff24a34e65d8e482866ebbb512b7104dc3523276
«TanueSha» <tvryabova@gmail.com>
2024-07-14 23:15:37 +0300

    lesson1:seminar1

---
+++ sem1.py
@@ -0,0 +1,10 @@
+# ввести два числа и определить какое из них больше
+num1 = int(input("input num1 "))
+num2 = int(input("input num2 "))
+if num1 > num2:
+    print(f"{num1} больше {num2}")
+    print(num1, 'num1 > num2', 'num1 =', num1, '>', 'num2 =',num2)
+    print(num1, 'num1 > num2', f'num1 = {num1} > num2 = {num2}')
+    
+
+    
